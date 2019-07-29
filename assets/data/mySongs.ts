0863b775650733daaaf9fca200899dc142fb0233
Brian-Holmes <52579801+Brian-Holmes@users.noreply.github.com>
2019-07-28 22:13:12 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,33 @@
+import {SongInfo} from '../../app/songInfo';
+
+export const MYSONGS: SongInfo[] = [
+    {
+        songname: 'C.R.E.A.M.',
+        artist: 'Wu-Tang Clan',
+        genre: 'Rap',
+        yearreleased: '1993', 
+        picture: './assets/images/wutangclan.jfif',     
+    },
+    {
+        songname: 'Hip Hop Is Dead',
+        artist: 'NAS',
+        genre: 'Hip Hop',
+        yearreleased: '2006', 
+        picture: './assets/images/nas.jfif',     
+    },
+    {
+        songname: 'Dead Wrong',
+        artist: 'The Notorious B.I.G.',
+        genre: 'Rap',
+        yearreleased: '1999', 
+        picture: './assets/images/biggie.jfif',     
+    },
+    {
+        songname: '99 Problems',
+        artist: 'Jay-Z',
+        genre: 'Rap',
+        yearreleased: '2003', 
+        picture: './assets/images/jayz.jfif',     
+    },
+    
+];
